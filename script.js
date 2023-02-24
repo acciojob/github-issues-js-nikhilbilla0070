d4fc8bf44a51d2df400364ebfd8ea5f5ebede4a2
nikhilbilla0070 <114826561+nikhilbilla0070@users.noreply.github.com>
2023-02-24 21:59:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 //your code here
+const issueList = document.getElementById('issue-list');
+    const pageNum = document.getElementById('page-num');
+    let currentPage = 1;
+
+    function displayIssues(issues) {
+      issueList.innerHTML = '';
+      issues.forEach(issue => {
+        const listItem = document.createElement('li');
+        listItem.innerText = issue.title;
+        issueList.appendChild(listItem);
+      });
+    }
+
+    function fetchIssues(pageNumber) {
+      fetch(`https://api.github.com/repositories/1296269/issues?page=${pageNumber}&per_page=5`)
+        .then(response => response.json())
+        .then(data => {
+          displayIssues(data);
+          currentPage = pageNumber;
+          pageNum.innerText = `Page number ${currentPage}`;
+        })
+        .catch(error => console.log(error));
+    }
+
+    // Initial page load
+    fetchIssues(currentPage);
+
+    // Load next page
+    document.getElementById('load-next').addEventListener('click', () => {
+      fetchIssues(currentPage + 1);
+    });
+
+    // Load previous page
+    document.getElementById('load-prev').addEventListener('click', () => {
+      if (currentPage > 1) {
+        fetchIssues(currentPage - 1);
+      }
+    });
